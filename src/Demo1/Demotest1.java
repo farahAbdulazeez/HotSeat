92f5f064d5fbdb5939e4a74049f56d60c3de9974
Farah <Farah@192.168.12.151>
2025-04-25 13:21:57 -0500

    added a new code1

--- src/Demo1/Demotest1.java
+++ src/Demo1/Demotest1.java
@@ -1,5 +1,14 @@
 package Demo1;
 
 public class Demotest1 {
+	public static void main (String args []) {
+		
+	
 
+	int a = 10;
+	int b = 20;
+	
+	int result = a + b;
+	System.out.println(result);
+}
 }
